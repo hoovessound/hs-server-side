0ad8f5cb97e0e9f62d5da4986496132fce7d83a6
Felix Fong <moongod101@hotmail.com>
2017-07-24 22:08:05 +0800

    add the widget router

---
+++ src/router/widget.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const rp = require('request-promise');
+const fullurl = require('fullurl');
+const Tracks = require('../../schema/Tracks');
+
+router.get('/:id?', (req, res) => {
+    const full_address = req.protocol + "://" + req.headers.host;
+    const id = req.params.id;
+    Tracks.findById(id).then(track => {
+        res.render('widget', {
+            track,
+            full_address,
+        });
+    }).catch(error => {
+        if(error.message.includes('Cast to ObjectId failed for value')){
+            res.render('widget', {
+                track: null,       
+            });
+        }else{
+            console.log(error)
+        }
+    })
+});
+
+module.exports = router;

2f32267a80ee3f4ebcaa8276b8a109b9f042dee1
Felix Fong <moongod101@hotmail.com>
2017-07-27 02:55:27 +0800

    add the project id to the gcloud SDK

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,8 @@ bash <( curl https://sdk.cloud.google.com) --disable-prompts
 # Aauthenticate the development bot
 /home/travis/google-cloud-sdk/bin/gcloud auth activate-service-account $EMAIL --key-file gcsAuth/gcsAuthToken.json
 
+# Settings up the gcloud command
+/home/travis/google-cloud-sdk/bin/gcloud config set project $PROJECT_ID
+
 # Deploy the bot
 /home/travis/google-cloud-sdk/bin/gcloud app deploy

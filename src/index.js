ec10c0b4b281b1d44f675795a7fb460d51df92d7
Felix Fong <moongod101@hotmail.com>
2017-08-04 16:50:44 +0800

    using the environment variables as a config file

--- src/index.js
+++ src/index.js
@@ -21,14 +21,8 @@ cli
     .option('--port [number]', 'The port that HoovesSound will running on')
     .parse(process.argv);
 
-// App init checking
-const db = require('./db');
-
 // Setting up the app port
 const port = process.env.PORT || cli.port || 3000;
-
-// GCS Auth Token Path
-module.exports.gcsPath = path.join(`${__dirname}/../gcsAuth/gcsAuthToken.json`)
 
 // Check of require directory
 fsp.exists(path.join(`${__dirname}/../usersContent`)).then(exists => {
@@ -52,6 +46,26 @@ fsp.exists(path.join(`${__dirname}/../tracks`)).then(exists => {
 }).catch(error => {
     console.log(error);
 });
+
+// If the run time is on production
+if(process.env.NODE_ENV) {
+    // Create the config file
+    console.log(`Node environment ${color.green('IS')} ${color.yellow('PRODUCTION')}`);
+    console.log('Creating config file');
+    console.log('Creating src/db.js');
+    fsp.writeFileSync(path.join(`${__dirname}/db.js`), process.env.DBJS);
+    console.log('Creating gscAuth/gscAuthToken.json');
+    fsp.writeFileSync(path.join(`${__dirname}/../gscAuth/gscAuthToken.json`), process.env.GCS_KEY_FILE);
+    console.log(`App config files is ${color.green('READY')}`);
+}else{
+    console.log(`Node environment is ${color.blue('NOT')} ${color.yellow('PRODUCTION')}`);
+    console.log('Using local db.js and gacAuthToke.json');
+}
+
+// GCS Auth Token Path
+module.exports.gcsPath = path.join(`${__dirname}/../gcsAuth/gcsAuthToken.json`);
+// App init checking
+const db = require('./db');
 
 server.listen(port, () => {
     console.log(`HoovesSound are running on port ${color.yellow(port)}`);

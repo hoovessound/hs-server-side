bb06e20463ddb901e516c5eaace210c52b1c360f
Felix Fong <moongod101@hotmail.com>
2017-08-28 19:27:11 +0800

    fixed small issue

--- src/router/view/track.js
+++ src/router/view/track.js
@@ -19,7 +19,7 @@ const escape = require('escape-html');
 const TextFormattign = {
     url: (text) => {
         const regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/igm;
-        if(typeof text !== 'undefined'){
+        if(typeof text !== 'undefined' && text !== null){
             if(text.match(regex)){
                 const url = text.match(regex);
                 return text.replace(url, `<a href="${url}" target="_blank">${url}</a>`);

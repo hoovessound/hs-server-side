00a77aa452aa949574380074d8f1403944299fb1
Felix Fong <moongod101@hotmail.com>
2017-09-20 18:25:54 +0800

    fixed the front end fave button

--- staticDest/js/track.js
+++ staticDest/js/track.js
@@ -8,16 +8,13 @@ faveButton.addEventListener('click', function (e) {
     var el = e.target;
     ajax.open('POST', '/api/track/fave/' + trackid + '?bypass=true');
     ajax.setRequestHeader('token', token);
+    ajax.setRequestHeader('sessionToken', sessionToken);
     ajax.send();
-    ajax.onload = function () {
-        if (ajax.status === 200 && ajax.readyState === 4) {
-            if (el.classList.contains('isFave')) {
-                el.classList.remove('isFave');
-            } else {
-                el.classList.add('isFave');
-            }
-        }
-    };
+    if (el.classList.contains('isFave')) {
+        el.classList.remove('isFave');
+    } else {
+        el.classList.add('isFave');
+    }
 });
 
 shareButton.addEventListener('click', function (e) {

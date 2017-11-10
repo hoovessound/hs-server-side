198f5c7f3c490a95ab33b33a4ac7276206fa0ef0
Felix Fong <moongod101@hotmail.com>
2017-11-10 19:21:17 +0800

    add a loading spinner

--- static/js/angular/index.js
+++ static/js/angular/index.js
@@ -11,12 +11,16 @@ var hsAngular = angular.module('hs', [
 
 hsAngular.run(function($rootScope) {
     $rootScope.$on("$locationChangeStart", function(event, next, current) {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
         localPlayList.clearTrack();
+        $('#loading').slideDown()
     });
 });
 
 hsAngular.run(function($rootScope, $templateCache) {
     $rootScope.$on('$viewContentLoaded', function() {
         $templateCache.removeAll();
+        $('#loading').slideUp()
     });
 });
--- staticDest/js/angular/index.js
+++ staticDest/js/angular/index.js
@@ -4,12 +4,16 @@ var hsAngular = angular.module('hs', ['hsRoutes', 'hsSearch', 'hsEditTracks', 'h
 
 hsAngular.run(function ($rootScope) {
     $rootScope.$on("$locationChangeStart", function (event, next, current) {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
         localPlayList.clearTrack();
+        $('#loading').slideDown();
     });
 });
 
 hsAngular.run(function ($rootScope, $templateCache) {
     $rootScope.$on('$viewContentLoaded', function () {
         $templateCache.removeAll();
+        $('#loading').slideUp();
     });
 });

bfea4e8e6c11b0954623f5afa6cc7499de348556
Felix Fong <moongod101@hotmail.com>
2017-11-10 19:39:11 +0800

    rewrite the tracks API a little bit

--- API/home.js
+++ API/home.js
@@ -6,64 +6,48 @@ const Tracks = require('../schema/Tracks');
 router.get('/', (req, res) => {
 
     const offset = parseInt(req.query.offset) || 0;
+    // Find tracks
+    Tracks.find({
+        private: false,
+    })
+    .limit(10)
+    .skip(offset)
+    .sort({uploadDate: -1})
+    .then(tracks => {
 
-    class TracksClass {
-        async findTracksAndResponse(){
-            try{
-                // Find the the latest 10 tracks order by the upload date
-                const tracks = await Tracks.find({
-                    $or: [
-                        {
-                            private: false,
-                        },
-                        {
-                            private: {
-                                $exists: false,
-                            }
-                        }
-                    ]
-                }, {
-                    file: 0,
-                    _id: 0,
-                    __v: 0,
-                    comments: 0,
-                    fave: 0,
-                }).limit(10).skip(offset).sort({uploadDate: -1});
-
-                const total = await Tracks.count({});
-                
-                tracks.forEach((track, index) => {
-                    return Users.findOne({
-                        id: track.author,
-                    })
-                    .then(user => {
-                        const username = user.username;
-                        const fullName = user.fullName;
-                        tracks[index].author = {
-                            username,
-                            fullName,
-                        };
-                        if(tracks.length === (index + 1) ){
-                            // Finish
-                            res.json({
-                                tracks,
-                                total,
-                            })
-                        }
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    })
-                })
-            }
-            catch(error){
-                console.log(error);
-            }
+        if(tracks.length <= 0){
+            res.json({
+                tracks: [],
+            })
+            return false;
         }
-    }
-
-    const tracks = new TracksClass();
-    tracks.findTracksAndResponse();
-
+        let finish = 0;
+        tracks.forEach((track, index) => {
+            return Users.findOne({
+                id: track.author,
+            })
+            .then(user => {
+                const username = user.username;
+                const fullName = user.fullName;
+                tracks[index].author = {
+                    username,
+                    fullName,
+                };
+                finish++;
+                if(tracks.length === finish ){
+                    // Finish
+                    res.json({
+                        tracks,
+                    })
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
 });
 module.exports = router;

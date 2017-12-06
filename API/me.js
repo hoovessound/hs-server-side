29a86d2f7d95cedb229dd82c10d15d2fffa9708b
Felix Fong <moongod101@hotmail.com>
2017-12-06 17:16:34 +0800

    created a new me/favorites router

--- API/me.js
+++ API/me.js
@@ -25,12 +25,46 @@ class Me {
         });
     }
 
+    async findMyFavorites(){
+        const user = this.req.hsAuth.user;
+        const req = this.req;
+        const res = this.res;
+        const responseObject = []
+        // Find the user favorite track IDs
+        for(let faveId of user.fave){
+            const track = await Tracks.findOne({id: faveId})
+            // Fetch the author info
+            const author = await Users.findOne({id: track.author});
+            track.author = {
+                username: author.username,
+                fullname: author.fullName,
+            }
+            responseObject.push({
+                id: track.id,
+                title: track.title,
+                uploadDate: track.uploadDate,
+                author: track.author,
+                description: track.description,
+                tags: track.tags,
+                private: track.private,
+                coverImage: track.coverImage,
+            })
+        }
+        res.json(responseObject)
+    }
+
 }
 
 router.get('/', (req, res) => {
     const token = req.body.token || req.headers.token || req.query.token;
     const me = new Me(req, res);
     me.findThisUserTracks();
+});
+
+router.get('/favorites', (req, res) => {
+    const token = req.body.token || req.headers.token || req.query.token;
+    const me = new Me(req, res);
+    me.findMyFavorites();
 });
 
 module.exports = router;

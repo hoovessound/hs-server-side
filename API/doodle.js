109e03378a8f8a434f970b0d1a0c444ee1296ee6
Felix Fong <moongod101@hotmail.com>
2017-12-17 03:24:08 +0800

    add a new collections of doodles API

--- API/doodle.js
+++ API/doodle.js
@@ -19,16 +19,48 @@ async function fetchArtWork() {
     }
 }
 
+async function fetchArtWorks(skip, isRandom) {
+    try{
+        const limit = 5;
+        const count = await Doodles.count();
+        const random = Math.floor(Math.random() * count);
+        let skipNumber = parseInt(isRandom ? random : skip);
+        const offlimit = parseInt(skipNumber + limit);
+        if(offlimit > count){
+            skipNumber = count - limit;
+        }
+        const artWork = await Doodles.find({}, {
+            id: 1,
+            image: 1,
+            used: 1,
+            author: 1,
+            _id: 0,
+
+        }).skip(skipNumber).limit(limit);
+        return(artWork)
+    }
+    catch (e){
+        console.log(e)
+    }
+}
+
 class Doodle {
     constructor(req, res){
         this.res = res;
         this.req = req;
     }
 
-    async findDoodles(){
+    async findDoodle(){
         const res = this.res;
         const req = this.req;
         const artWork = await fetchArtWork()
+        res.json(artWork);
+    }
+    
+    async findDoodles(skip){
+        const res = this.res;
+        const req = this.req;
+        const artWork = await fetchArtWorks(skip, req.query.random)
         res.json(artWork);
     }
 
@@ -36,7 +68,13 @@ class Doodle {
 
 router.get('/', (req, res) => {
     const doodle = new Doodle(req, res);
-    doodle.findDoodles();
+    doodle.findDoodle();
+});
+
+router.get('/collections/:skip?', (req, res) => {
+    const skip = req.params.skip || 0;
+    const doodle = new Doodle(req, res);
+    doodle.findDoodles(skip);
 });
 
 module.exports = router;
